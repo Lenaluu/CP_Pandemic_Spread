78c9d40802a6f6f7a20f983270ad28e048cb4860
Lena <lena@lindus.de>
2021-05-04 16:50:24 +0200

    Task 1: done.

--- Pandemic_Spread.py
+++ Pandemic_Spread.py
@@ -10,54 +10,94 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-######### Task 1 ##########
 
-def Task_1(T, No=20, r=0.1):
-    for s in range(3):
+def Task_1(T, S):
+    # initial parameters
+    No = 20  # time span
+    r = 0.1  # decay rate
+
+    # logarithmic model
+    y_log = N_log(T, r, No)
+
+    # algorithm realizations
+    Y_alg = []
+    for s in range(S):
         random.seed(s)
-        plot_models(T, No, r, 'death process')
+        Y_alg.append(N_alg(T, r, No))
+    Y_alg = np.array(Y_alg)
 
-def plot_models(T, No, r, title):
+    # plotting
+    plot_models(T, S, No, y_log, Y_alg,
+                title='death process with rate $r={}$'.format(r))
+
+
+def Task_2(T, S):
+    pass
+
+
+def N_log(T, r, No):  # logarithmic model
+    N = [No]
+    for t in range(T):
+        N.append(N[-1] - r*N[-1])  # mean field eq: N(t+1) = N(t) + dN(t)/dt
+    return np.array(N)
+
+
+def N_alg(T, r, No):  # algorithm realizations
+    N = [No]
+    for t in range(T):
+        # num = k-sized list of population elements chosen with replacement
+        num = random.choices([0, 1], weights=[r, 1-r], k=N[-1])
+        N.append(int(np.sum(num)))  # sum = new population size
+    return np.array(N)
+
+
+def plot_models(T, S, No, y_log, Y_alg, title):
+    """
+    Parameters
+    ----------
+    T : int
+        Time span.
+    S : int
+        Number of sample realizations.
+    No : int
+        Initial number of particles N_0.
+    y_log : 1D array
+        Logarithmic model from mean-field equations.
+    Y_alg : 2D array
+        Gillespie algorithm realizations from Marcov process.
+    title : str
+        Plot title.
+    """
+
+    fig, ax = plt.subplots(figsize=(8.4, 4.8))
     x = np.arange(0, T+1)
-    y_log = np.array(N_log(T, r, No))
-    y_alg = np.array(N_alg(T, r, No))
-    
-    fig, ax = plt.subplots()
-    plt.plot(x, y_log, c='tab:blue', lw=2,
-             label='mean-field equations (logarithmic model)')
-    plt.plot(x, y_alg, 'o', c='tab:orange', zorder=-1,
-             label='Marcov process (Gillespie algorithm)')
+
+    # logarithmic model
+    l1, = plt.plot(x, y_log, lw=2, c='k',
+                   label='mean-field equations (logarithmic model)')
+    # algorithm realizations
+    for s in range(S):
+        l2, = plt.plot(x, Y_alg[s], 'o-', ms=2, zorder=-1,
+                       label='Marcov process (Gillespie algorithm)')
+    # algorithm mean+std
+    Y_alg_mean = np.mean(Y_alg, axis=0)
+    Y_alg_std = np.std(Y_alg, axis=0)
+    l3 = plt.errorbar(x, Y_alg_mean, fmt='o', c='k', ms=5, yerr=Y_alg_std,
+                      label=r'$\left< N(t) \right>$ mean+std of '
+                            + str(S)+' Markov realizations')
+    # layout
     plt.xlabel('time')
-    plt.ylabel('population')
+    plt.ylabel('number of particles $N$')
     plt.title(title)
-    plt.legend()
-    ax2 = ax.twinx()
-    ax2.set_ylim(ax.get_ylim())
+    plt.legend(handles=[l1, l2, l3])
+    plt.yticks(np.arange(0, No+2, step=np.round(No/10)))  # only integers
+    ax2 = ax.twinx()  # secondary axis for additional labels
+    ax2.set_ylim(ax.get_ylim())  # same ylim
     ax2.set_yticks([0, No])
-    ax2.set_yticklabels(['0', r'$N_0$'])
+    ax2.set_yticklabels(['0', '$N_0$'])
     plt.show()
-    
-def N_log(T, r, No):
-    N = [No]
-    for t in range(T):
-        N.append(N[-1] + dNdt_log(r, N[-1]))
-    return N
-
-def dNdt_log(r, N):
-    dNdt = -r*N
-    return dNdt
-
-def N_alg(T, r, No):
-    N = [No]
-    for t in range(T):
-        N.append(dNdt_alg(r, N[-1]))
-    return N
-
-def dNdt_alg(r, N):
-    num = random.choices([0,1], weights=[r, 1-r], k=N)
-    dNdt = int(np.sum(num))
-    return dNdt
 
 
 if __name__ == '__main__':
-    Task_1(60)
+    Task_1(T=70, S=70)
+    Task_2(T=10, S=10)
